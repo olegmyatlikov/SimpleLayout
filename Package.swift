e80e63ee9a6956d1734e138ab8e5d11f437ba8e8
Oleg Myatlikov <oleg.myatlikov@p97.com>
2021-02-17 18:55:45 +0100

    add swift package manager supporting

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+//import PackageDescription
+
+let package = Package(
+    name: "SimpleLayout",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(name: "SimpleLayout",
+                 targets: ["SimpleLayout"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "SimpleLayout",
+                dependencies: [],
+                path: "Sources")
+    ]
+)
